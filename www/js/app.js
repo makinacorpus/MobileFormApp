b532c87a6469a893a10a6567c8025c10bcee9a75
Simon Prévidente <simon.previdente@free.fr>
2015-05-19 14:27:51 +0200

    Refactor json data path

--- www/js/app.js
+++ www/js/app.js
@@ -1,59 +1,18 @@
 angular.module('mobileformapp', ['ionic','schemaForm'])
 
-	.controller('FormController', function($scope) {
-		$scope.schema = {
-			"type": "object",
-			"title": "Comment",
-			"properties": {
-				"name": {
-					"title": "Name",
-					"type": "string"
-				},
-				"email": {
-					"title": "Email",
-					"type": "string",
-					"pattern": "^\\S+@\\S+$",
-					"description": "Email will be used for evil."
-				},
-				"comment": {
-					"title": "Comment",
-					"type": "string",
-					"maxLength": 20,
-					"validationMessage": "Don't be greedy!"
-				}
-			},
-			"required": [
-				"name",
-				"email",
-				"comment"
-			]
-		};
-		$scope.form = [
-			{
-				"key": "name",
-				"placeholder": "Leeroy Jenkins",
-				// "htmlClass": "item item-input",
-				// "labelHtmlClass": "input-label"
-			},
-			{
-				"key": "email",
-				"placeholder": "leeroy@palsforlife.com",
-				// "htmlClass": "item item-input",
-				// "labelHtmlClass": "input-label"
-			},
-			{
-				"key": "comment",
-				"type": "textarea",
-				"placeholder": "At least i have chicken.",
-				// "htmlClass": "item item-input",
-				// "labelHtmlClass": "input-label"
-			},
-			{
-				"type": "submit",
-				"style": "btn-info",
-				"title": "Send",
-				//"fieldHtmlClass": "button button-positive"
-			}
-		];
+	.controller('FormController', function($http, $scope) {
+		// Schema
+		$scope.schema = {};
+		$http.get('mock_schema.json')
+			.success(function(data, status, headers, config) {
+				$scope.schema = data;
+			});
+		// Form
+		$scope.form = [];
+		$http.get('mock_form.json')
+			.success(function(data, status, headers, config) {
+				$scope.form = data;
+			});
+		// Model
 		$scope.model = {};
 	});
