419e8cd7f13baa368d5528003bcfc3600cdd908b
Simon Prévidente <simon.previdente@free.fr>
2015-05-22 16:34:52 +0200

    Fix bug reset after install

--- gulp/tasks/ionic.js
+++ gulp/tasks/ionic.js
@@ -2,19 +2,19 @@ var gulp = require("gulp");
 var sh = require("shelljs");
 
 gulp.task("ionic", ["bower-install"], function() {
-	// Platform
-	if(process.argv.indexOf("--os") > 0) {
-		var os = process.argv[process.argv.indexOf("--os")+1]
-		if(os == "all") {
-			sh.exec("ionic platform add android --nosave");
-			sh.exec("ionic platform add ios --nosave");
-		} else {
-			sh.exec("ionic platform add "+ os +" --nosave");
-		}
-	} else {
-		sh.exec("ionic platform add android --nosave");
-	}
-
 	// Plugins
 	sh.exec("ionic state reset");
+
+  // Platform
+  if(process.argv.indexOf("--os") > 0) {
+    var os = process.argv[process.argv.indexOf("--os")+1]
+    if(os == "all") {
+      sh.exec("ionic platform add android --nosave");
+      sh.exec("ionic platform add ios --nosave");
+    } else {
+      sh.exec("ionic platform add "+ os +" --nosave");
+    }
+  } else {
+    sh.exec("ionic platform add android --nosave");
+  }
 });
