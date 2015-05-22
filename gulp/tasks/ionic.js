fed0bd3a2bdace91bd70fa3fd267f8f1ed076b78
Simon Prévidente <simon.previdente@free.fr>
2015-05-22 10:50:52 +0200

    Fix install with no parameters

--- gulp/tasks/ionic.js
+++ gulp/tasks/ionic.js
@@ -3,12 +3,16 @@ var sh = require("shelljs");
 
 gulp.task("ionic", ["bower-install"], function() {
 	// Platform
-	var os = process.argv[process.argv.indexOf("--os")+1]
-	if(os == "all") {
-		sh.exec("ionic platform add andoid");
-		sh.exec("ionic platform add ios");
+	if(process.argv.indexOf("--os") > 0) {
+		var os = process.argv[process.argv.indexOf("--os")+1]
+		if(os == "all") {
+			sh.exec("ionic platform add andoid");
+			sh.exec("ionic platform add ios");
+		} else {
+			sh.exec("ionic platform add "+ os);
+		}
 	} else {
-		sh.exec("ionic platform add "+ os);
+		sh.exec("ionic platform add andoid");
 	}
 
 	// Plugins
