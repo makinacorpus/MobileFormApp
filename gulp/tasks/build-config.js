a7f9482c92755b7f39e85924dc6e0d14affe42c2
Simon Prévidente <simon.previdente@free.fr>
2015-05-22 16:21:22 +0200

    Generate config json file with Gulp

--- gulp/tasks/build-config.js
+++ gulp/tasks/build-config.js
@@ -1,17 +1,25 @@
 var gulp = require("gulp");
+var sh = require("shelljs");
+var SimpleFileWriter = require('simple-file-writer');
 var jeditor = require("gulp-json-editor");
 
 var config = require("../config").MobileFormApp.config;
 
 gulp.task("build-config", function() {
-	var url = "";
+	// json config file
+	sh.exec("rm "+ config.src);
+	var writer = new SimpleFileWriter(config.src);
+	writer.write("{\n	\n}");
 
+	// url param
+	var url = "";
 	if(process.argv.indexOf("--url") > 0) {
 		url = process.argv[process.argv.indexOf("--url")+1];
 	} else {
 		url = config.default_url;
 	}
 
+	// json data
 	return gulp
 		.src(config.src)
 		.pipe(jeditor({'url': url}))
