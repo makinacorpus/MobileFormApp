76ae1c434e7173223fee57a43f2c35e71f9a1301
Simon Prévidente <simon.previdente@free.fr>
2015-06-09 11:10:00 +0200

    Add url case in daybed to angular schema form parser

--- www/js/app.js
+++ www/js/app.js
@@ -49,7 +49,12 @@ angular.module('mobileformapp', ['ionic','schemaForm'])
 					break;
 				case "email":
 					fieldSchema.type = "string";
-					fieldSchema.pattern = "^\\S+@\\S+$";
+					fieldSchema.pattern = "[\w-]+@([\w-]+\.)+[\w-]+";
+					fieldForm.key = field.name;
+					break;
+				case "url":
+					fieldSchema.type = "string";
+					fieldSchema.pattern = "^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$";
 					fieldForm.key = field.name;
 					break;
 				case "text":
